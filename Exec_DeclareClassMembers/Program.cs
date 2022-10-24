73f0c6566acd340dece57e3aa8594686d97485a7
88693 <88693@LAPTOP-FS9ST1LD>
2022-10-24 23:12:42 +0800

    加入專案檔案。

---
+++ Exec_DeclareClassMembers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareClassMembers
+{/// <summary>
+/// Q1 將指定文字內容存檔, 由於要存放的文字內容與檔案存放位置都會變化, 
+/// 所以我們可以將這兩項資訊設計成 method的參數, 由外界傳入請設計一個FileManager class, 
+/// 內含一個 Save method只需要設定method, 不必實作出功能
+/// </summary>
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			FileManager answer=new FileManager();
+			answer.Save("123.txt", "C:\\Users\\88693");
+		}
+	}
+
+	class FileManager
+	{
+		public void Save(string FileName, string FilePath) 
+		{
+			//ToDo	
+		}
+
+	}
+}
